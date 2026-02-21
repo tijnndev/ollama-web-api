b0510292f79bf5d782df65b00031d68e89f64d91
Tijn <tijnnotkamp@gmail.com>
2026-02-21 01:20:27 +0100

    Improves authentication handling
    Extends authentication handling to include handling of 502 errors, which will log the user out if they encounter one.
    
    Adds a systemd service file for managing the web API as a service.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -19,7 +19,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 || error?.response.status === 502) {
       localStorage.removeItem('token');
       window.dispatchEvent(new Event('auth:unauthorized'));
     }
